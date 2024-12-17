d8917dfc3c52b58387e2c9f3bf403a1898b02aa5
dinesh-tech-757 <dineshmoorthi757@gmail.com>
2024-12-17 17:39:43 +0530

    change to cpanel path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zeos-pitshop/"
+  base: "/dinesh.ashwinsrivastava.com/public_html/"
 })
