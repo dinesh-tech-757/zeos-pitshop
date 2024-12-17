4a309ffcd7c715dbdfbeb613ac860b3ec3693218
dinesh-tech-757 <dineshmoorthi757@gmail.com>
2024-12-17 17:45:22 +0530

    redo changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dinesh.ashwinsrivastava.com/public_html/"
+  base: "/zeos-pitshop/"
 })
